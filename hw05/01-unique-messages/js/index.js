55227e9aa711e547028fab1b45c3e248a9fc4ded
Harcourt Allen <harcourtallen@dhcp-10-105-67-100.wireless.northwestern.private>
2019-05-07 15:41:38 -0500

    not 6 or 10 or 12

---
+++ hw05/01-unique-messages/js/index.js
@@ -0,0 +1,19 @@
+const names = [
+    "Jane", "Brenda", "Wanda", "Maria", "Jasper",
+    "John", "Malik", "Arjun", "Larry", "Curly", "Moe"
+];
+
+
+// use a for / of loop to loop through each element
+// of the names array and output a message to the screen:
+for (i = 0; i < names.length; i++) {
+  document.write("Welcome, " + names[i] + "! Make yourself comfortable.<br>");
+
+}
+
+//OR
+
+for (name of names) {
+  document.write("Welcome, " + name + "! Make yourself comfortable.<br>");
+
+}
---
+++ hw05/05-gallery-revision/js/index.js
@@ -0,0 +1,69 @@
+// const loadCards = (photos) => {
+//     console.log(photos);
+//     let template = `
+//         <li class="card">
+//             <div class="image" style="background-image:url('${photos[0]}')"></div>
+//         </li>
+//     `;
+//     document.querySelector('.cards').innerHTML += template;
+//
+//     template = `
+//         <li class="card">
+//             <div class="image" style="background-image:url('${photos[1]}')"></div>
+//         </li>
+//     `;
+//     document.querySelector('.cards').innerHTML += template;
+//
+//     template = `
+//         <li class="card">
+//             <div class="image" style="background-image:url('${photos[2]}')"></div>
+//         </li>
+//     `;
+//     document.querySelector('.cards').innerHTML += template;
+// };
+
+// for (i = 0; i < loadCards.length; i++) {
+//
+//   const loadCards = (photos) => {
+//       console.log(photos);
+//   let template = `
+//       <li class="card">
+//           <div class="image" style="background-image:url('${photos[i]}')"></div>
+//       </li>
+//   `;
+//   document.querySelector('.cards').innerHTML += template;
+// }
+// }
+
+const loadCards = (photos) => {
+  console.log(photos);
+  for (i = 0; i < photos.length; i++) {
+    let template = `
+      <li class="card">
+        <div class="image" style="background-image:url('${photos[i]}')"></div>
+      </li>
+    `;
+    document.querySelector('.cards').innerHTML += template;
+  }
+}
+
+
+loadCards([
+    'images/poppies.jpg',
+    'images/dogwoods.jpg',
+    'images/blossom.jpg',
+    'images/field3.jpg',
+    'images/field4.jpg',
+    'images/branch.jpg',
+    'images/red.jpg',
+    'images/purple2.jpg',
+    'images/field1.jpg',
+    'images/purple.jpg',
+    'images/jar.jpg',
+    'images/green.jpg',
+    'images/green1.jpg',
+    'images/purple1.jpg',
+    'images/magnolias.jpg',
+    'images/daisy1.jpg'
+]);
+initCarousel();
