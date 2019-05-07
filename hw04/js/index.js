3daa7d2df6805a1805c7cc3f8fa461a527588ab2
Harcourt Allen <harcourtallen@dhcp-10-105-67-100.wireless.northwestern.private>
2019-05-07 15:38:38 -0500

    updated that now works

--- hw04/js/index.js
+++ hw04/js/index.js
@@ -1,5 +1,8 @@
+let activeImage = null;
+
 const showPhoto = (e) => {
     const clickedElement = e.target; // figure out which element the user clicked from the event object:
+    activeImage = clickedElement;
     const imgURL = clickedElement.style.backgroundImage; // figure out what its background image is:
     console.log(imgURL);
     // PART 1:
@@ -22,32 +25,43 @@ document.querySelector('.close').onclick = close;
 // PART 4: this doesn't work and i was gone all week sat-following sun so no office hours
 
 
-var result = document.getElementsByClassName('featured_image');
-var nextt = result.nextSiblingElement;
+// var result = document.getElementsByClassName('cards');
+// var nextt = result.nextSiblingElement;
 
 const next = (e) => {
-  const clickedElement = e.target;
-  const imgURL = clickedElement.style.backgroundImage;
-  console.log(imgURL);
-  document.querySelector('.featured_image').style.backgroundImage = imgURL.nextt;
+  if (!activeImage.parentElement.nextElementSibling) {
+    activeImage = document.querySelector('.image');
+  }
+  else {
+    activeImage = activeImage.parentElement.nextElementSibling.firstElementChild;
+  }
+  // const clickedElement = e.target;
+  // const imgURL = clickedElement.style.backgroundImage;
+  // console.log(imgURL);
+  // document.querySelector('.featured_image').style.backgroundImage = imgURL.nextt;
   //document.querySelector('.featured_image').style.backgroundImage = imgURL.nextSiblingElement;
+
+  document.querySelector('.featured_image').style.backgroundImage = activeImage.style.backgroundImage;
+
 }
 document.querySelector('.next').onclick = next;
 document.querySelector('.featured_image').onclick = next;
 
 
 // PART 5:
-var prevv = result.previousSiblingElement;
+// var prevv = result.previousSiblingElement;
 
 const previous = (e) => {
-  const clickedElement = e.target;
-  const imgURL = clickedElement.style.backgroundImage;
-  console.log(imgURL);
-  document.querySelector('.featured_image').style.backgroundImage = imgURL.prevv;
-  //document.querySelector('.featured_image').style.backgroundImage = imgURL.previousSiblingElement;
+  const num_images = document.querySelectorAll('.image').length;
+  if (!activeImage.parentElement.previousElementSibling) {
+  activeImage = document.querySelectorAll('.image')[num_images-1];
+  }
+  else {
+    activeImage = activeImage.parentElement.previousElementSibling.firstElementChild;
+  }
+  document.querySelector('.featured_image').style.backgroundImage = activeImage.style.backgroundImage;
 }
 document.querySelector('.prev').onclick = previous;
-
 
 
 const biggerPic = () => {
